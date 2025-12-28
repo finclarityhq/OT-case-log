5a5a1ff3c428176c8cd596ae7b722d292731819d
finclarityhq <rahulbsws098@gmail.com>
2025-12-28 20:59:11 +0530

    Update config.ts
    use environment variables for Firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-6765925778-47328",
-  "appId": "1:157318334484:web:4371814b5dd53533bcff01",
-  "apiKey": "AIzaSyAJIWNlnn8QyyGnDNcJpiNwZVmi0qUi4DA",
-  "authDomain": "studio-6765925778-47328.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "157318334484"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
